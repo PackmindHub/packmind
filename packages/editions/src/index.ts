6e6274ebb32d79bae7d3d43fc0f856aa3a095aba
Malo <malo.couaran@promyze.com>
2025-11-06 17:52:46 +0100

    :bug: missing stub for linter ast

--- packages/editions/src/index.ts
+++ packages/editions/src/index.ts
@@ -1,4 +1,5 @@
 export * from './oss/analytics';
 export * from './oss/linter';
 export * from './oss/linter-execution';
+export * from './oss/linter-ast';
 export * from './oss/amplitude';
---
+++ packages/editions/src/oss/linter-ast/index.ts
@@ -0,0 +1,8 @@
+export abstract class BaseParser {
+  public static setWasmDirectory(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _directory: string,
+  ): void {
+    // No-op
+  }
+}
