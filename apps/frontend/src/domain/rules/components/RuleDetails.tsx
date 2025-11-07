635e1ecafe832623f64487c6ee77c62de1edf86f
Steven <steven.drouet@promyze.com>
2025-11-07 10:03:32 +0100

    Remove detection tab feature flag

--- apps/frontend/src/domain/rules/components/RuleDetails.tsx
+++ apps/frontend/src/domain/rules/components/RuleDetails.tsx
@@ -1,15 +1,7 @@
-import {
-  DEFAULT_FEATURE_DOMAIN_MAP,
-  PMPageSection,
-  PMTabs,
-  PMVStack,
-  RULE_DETAILS_DETECTION_TAB_FEATURE_KEY,
-  isFeatureFlagEnabled,
-} from '@packmind/ui';
+import { PMPageSection, PMTabs, PMVStack } from '@packmind/ui';
 import { Rule, StandardId } from '@packmind/types';
 import { RuleExamplesManager } from './RuleExamplesManager';
 import { ProgramEditor } from '@packmind/proprietary/frontend/domain/detection/components/ProgramEditor';
-import { useAuthContext } from '../../accounts/hooks';
 
 type RuleDetailsTab = 'examples' | 'detection';
 
@@ -26,14 +18,6 @@ export const RuleDetails = ({
   defaultTab = 'examples',
   detectionLanguages = [],
 }: RuleDetailsProps) => {
-  const { user } = useAuthContext();
-
-  const isDetectionTabEnabled = isFeatureFlagEnabled({
-    featureKeys: [RULE_DETAILS_DETECTION_TAB_FEATURE_KEY],
-    featureDomainMap: DEFAULT_FEATURE_DOMAIN_MAP,
-    userEmail: user?.email,
-  });
-
   const tabs = [
     {
       value: 'examples',
@@ -44,25 +28,21 @@ export const RuleDetails = ({
         </PMVStack>
       ),
     },
-    ...(isDetectionTabEnabled
-      ? [
-          {
-            value: 'detection',
-            triggerLabel: 'Linter',
-            content: (
-              <PMVStack alignItems={'stretch'} gap="4" paddingY={'4'}>
-                <PMPageSection>
-                  <ProgramEditor
-                    standardId={standardId}
-                    ruleId={rule.id}
-                    detectionLanguages={detectionLanguages}
-                  />
-                </PMPageSection>
-              </PMVStack>
-            ),
-          },
-        ]
-      : []),
+    {
+      value: 'detection',
+      triggerLabel: 'Linter',
+      content: (
+        <PMVStack alignItems={'stretch'} gap="4" paddingY={'4'}>
+          <PMPageSection>
+            <ProgramEditor
+              standardId={standardId}
+              ruleId={rule.id}
+              detectionLanguages={detectionLanguages}
+            />
+          </PMPageSection>
+        </PMVStack>
+      ),
+    },
   ];
 
   return <PMTabs defaultValue={defaultTab} tabs={tabs} />;
