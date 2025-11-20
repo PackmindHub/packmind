7168f7d79df4a25e445707d7737cc0eb6d0b006c
Malo <malo.couaran@promyze.com>
2025-11-20 02:56:41 +0100

    🐛 Export RagLabConfigurationSchema to fix TypeORM metadata error

--- packages/learnings/src/index.ts
+++ packages/learnings/src/index.ts
@@ -1,6 +1,7 @@
 import { TopicSchema } from './infra/schemas/TopicSchema';
 import { KnowledgePatchSchema } from './infra/schemas/KnowledgePatchSchema';
 import { TopicKnowledgePatchSchema } from './infra/schemas/TopicKnowledgePatchSchema';
+import { RagLabConfigurationSchema } from './infra/schemas/RagLabConfigurationSchema';
 
 export { LearningsHexa } from './LearningsHexa';
 export { LearningsAdapter } from './application/adapter/LearningsAdapter';
@@ -8,10 +9,12 @@ export { LearningsModule } from './learnings.module';
 export { TopicSchema };
 export { KnowledgePatchSchema };
 export { TopicKnowledgePatchSchema };
+export { RagLabConfigurationSchema };
 
 // Export schemas array for TypeORM DataSource registration
 export const learningsSchemas = [
   TopicSchema,
   KnowledgePatchSchema,
   TopicKnowledgePatchSchema,
+  RagLabConfigurationSchema,
 ];
