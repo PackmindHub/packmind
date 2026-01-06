e2310c2a2f5fe84cefdbfdd430e42e3435264f28
Vincent Pretre <vincent.pretre@gmail.com>
2026-01-06 15:55:22 +0100

    Found reference to rcipe in blank state #85

--- apps/frontend/src/domain/deployments/components/PackagesPage/PackagesPage.tsx
+++ apps/frontend/src/domain/deployments/components/PackagesPage/PackagesPage.tsx
@@ -237,7 +237,7 @@ export const PackagesPage: React.FC<PackagesPageProps> = ({
           mx={'auto'}
           title={'No packages yet'}
         >
-          Packages are collections of standards and recipes that can be
+          Packages are collections of standards and commands that can be
           distributed together to your repositories, ensuring consistent
           practices across your projects.
           <PMHStack>
