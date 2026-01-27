2d724cec2411213a22fe13efa59217c95aeb624f
Joan Racenet <joan.racenet@packmind.com>
2026-01-27 10:43:18 +0100

    :sparkles: Add splash screen placeholder to sign up flow (see onboarding-flow.md)

--- apps/frontend/app/routes/_public.sign-up.tsx
+++ apps/frontend/app/routes/_public.sign-up.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { useIsAuthenticated } from '../../src/domain/accounts/hooks/useIsAuthenticated';
+import { useState } from 'react';
+import { SignUpSplashScreen } from '../../src/domain/accounts/components/SignUpSplashScreen';
 import { SignUpOptions } from '../../src/domain/accounts/components/SignUpOptions';
 
 export default function SignUpRouteModule() {
   const { isAuthenticated } = useIsAuthenticated();
   const navigate = useNavigate();
+  const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -13,5 +16,13 @@ export default function SignUpRouteModule() {
     }
   }, [isAuthenticated, navigate]);
 
+  if (showSplash) {
+    return (
+      <SignUpSplashScreen
+        // On itèrera sur le contenu plus tard
+        onGetStarted={() => setShowSplash(false)}
+      />
+    );
+  }
   return <SignUpOptions />;
 }
