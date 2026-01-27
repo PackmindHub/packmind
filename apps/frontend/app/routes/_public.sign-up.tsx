e924a35a46e4cbc1c0019b16373a368c8684a8c5
Joan Racenet <joan.racenet@packmind.com>
2026-01-27 11:11:58 +0100

    :recycle: Restructure sign-up routes with splash screen and nested create-account form

--- apps/frontend/app/routes/_public.sign-up.tsx
+++ apps/frontend/app/routes/_public.sign-up.tsx
@@ -1,28 +1,5 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import { useIsAuthenticated } from '../../src/domain/accounts/hooks/useIsAuthenticated';
-import { useState } from 'react';
-import { SignUpSplashScreen } from '../../src/domain/accounts/components/SignUpSplashScreen';
-import { SignUpOptions } from '../../src/domain/accounts/components/SignUpOptions';
+import { Outlet } from 'react-router';
 
-export default function SignUpRouteModule() {
-  const { isAuthenticated } = useIsAuthenticated();
-  const navigate = useNavigate();
-  const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/');
-    }
-  }, [isAuthenticated, navigate]);
-
-  if (showSplash) {
-    return (
-      <SignUpSplashScreen
-        // On itèrera sur le contenu plus tard
-        onGetStarted={() => setShowSplash(false)}
-      />
-    );
-  }
-  return <SignUpOptions />;
+export default function SignUpLayout() {
+  return <Outlet />;
 }
