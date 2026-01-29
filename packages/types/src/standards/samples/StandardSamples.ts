61bbc9258f64d34c3f2776400b3f38d5e469956e
cteyton <cedric.teyton@packmind.com>
2026-01-29 16:17:50 +0100

    Enrich the database of languages and frameworks samples

--- packages/types/src/standards/samples/StandardSamples.ts
+++ packages/types/src/standards/samples/StandardSamples.ts
@@ -5,15 +5,155 @@ export type Sample = {
 
 const languageSamples: Sample[] = [
   {
+    id: 'python',
+    displayName: 'Python',
+  },
+  {
+    id: 'c',
+    displayName: 'C',
+  },
+  {
     id: 'java',
     displayName: 'Java',
+  },
+  {
+    id: 'cpp',
+    displayName: 'C++',
+  },
+  {
+    id: 'csharp',
+    displayName: 'C#',
+  },
+  {
+    id: 'javascript',
+    displayName: 'JavaScript',
+  },
+  {
+    id: 'sql',
+    displayName: 'SQL',
+  },
+  {
+    id: 'go',
+    displayName: 'Go',
+  },
+  {
+    id: 'php',
+    displayName: 'PHP',
+  },
+  {
+    id: 'rust',
+    displayName: 'Rust',
+  },
+  {
+    id: 'swift',
+    displayName: 'Swift',
+  },
+  {
+    id: 'typescript',
+    displayName: 'TypeScript',
+  },
+  {
+    id: 'kotlin',
+    displayName: 'Kotlin',
+  },
+  {
+    id: 'ruby',
+    displayName: 'Ruby',
   },
 ];
 
 const frameworkSamples: Sample[] = [
   {
-    id: 'spring',
-    displayName: 'Spring',
+    id: 'react',
+    displayName: 'React',
+  },
+  {
+    id: 'django',
+    displayName: 'Django',
+  },
+  {
+    id: 'angular',
+    displayName: 'Angular',
+  },
+  {
+    id: 'ansible',
+    displayName: 'Ansible',
+  },
+  {
+    id: 'terraform',
+    displayName: 'Terraform',
+  },
+  {
+    id: 'vue',
+    displayName: 'Vue.js',
+  },
+  {
+    id: 'spring-boot',
+    displayName: 'Spring Boot',
+  },
+  {
+    id: 'laravel',
+    displayName: 'Laravel',
+  },
+  {
+    id: 'flask',
+    displayName: 'Flask',
+  },
+  {
+    id: 'dotnet-core',
+    displayName: '.NET Core',
+  },
+  {
+    id: 'rails',
+    displayName: 'Ruby on Rails',
+  },
+  {
+    id: 'svelte',
+    displayName: 'Svelte',
+  },
+  {
+    id: 'symfony',
+    displayName: 'Symfony',
+  },
+  {
+    id: 'nextjs',
+    displayName: 'Next.js',
+  },
+  {
+    id: 'nuxtjs',
+    displayName: 'Nuxt.js',
+  },
+  {
+    id: 'nestjs',
+    displayName: 'Nest.js',
+  },
+  {
+    id: 'react-native',
+    displayName: 'React Native',
+  },
+  {
+    id: 'meteor',
+    displayName: 'Meteor',
+  },
+  {
+    id: 'fastapi',
+    displayName: 'FastAPI',
+  },
+  {
+    id: 'flutter',
+    displayName: 'Flutter',
+  },
+  {
+    id: 'zend',
+    displayName: 'Zend Framework',
+  },
+  {
+    id: 'express',
+    displayName: 'Express.js',
+  },
+  {
+    id: 'ionic',
+    displayName: 'Ionic',
   },
 ];
 
