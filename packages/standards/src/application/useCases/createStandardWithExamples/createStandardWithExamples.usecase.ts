f0f1f8ebf6ff07b7a7bba72b00c11088133ee87d
Malo <malo.couaran@promyze.com>
2026-02-04 17:27:57 +0100

    fix prettier error

--- packages/standards/src/application/useCases/createStandardWithExamples/createStandardWithExamples.usecase.ts
+++ packages/standards/src/application/useCases/createStandardWithExamples/createStandardWithExamples.usecase.ts
@@ -72,7 +72,7 @@ export class CreateStandardWithExamplesUsecase {
       spaceId,
       disableTriggerAssessment = false,
       source = 'ui',
-      method = 'blank', 
+      method = 'blank',
     } = command;
 
     const organizationId = createOrganizationId(orgId);
