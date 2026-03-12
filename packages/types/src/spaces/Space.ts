697be638783031d8ecc020f2e059ed35af482fc6
Vincent Pretre <vincent.pretre@gmail.com>
2026-03-12 15:14:40 +0100

    Added type field to Space

--- packages/types/src/spaces/Space.ts
+++ packages/types/src/spaces/Space.ts
@@ -1,9 +1,16 @@
 import { SpaceId } from './SpaceId';
 import { OrganizationId } from '../accounts/Organization';
 
+export enum SpaceType {
+  open = 'open',
+  restricted = 'restricted',
+  private = 'private',
+}
+
 export type Space = {
   id: SpaceId;
   name: string;
   slug: string;
+  type: SpaceType;
   organizationId: OrganizationId;
 };
