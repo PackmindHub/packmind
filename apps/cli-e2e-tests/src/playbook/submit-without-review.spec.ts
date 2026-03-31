c780a6c967600da39fce73dde211abef43f10315
Vincent Pretre <vincent.pretre@gmail.com>
2026-03-31 14:01:48 +0200

    Add E2E test to test artefact creation with playbook submit --no-review command

---
+++ apps/cli-e2e-tests/src/playbook/submit-without-review.spec.ts
@@ -0,0 +1,71 @@
+import {
+  describeWithUserSignedUp,
+  RunCliResult,
+  setupGitRepo,
+  updateFile,
+  UserSignedUpContext,
+} from '../helpers';
+import fs from 'fs';
+
+describeWithUserSignedUp('playbook submit --no-review', (getContext) => {
+  let context: UserSignedUpContext;
+
+  beforeEach(async () => {
+    context = await getContext();
+    await setupGitRepo(context.testDir);
+
+    updateFile(
+      'packmind.json',
+      JSON.stringify({ packages: {} }),
+      context.testDir,
+    );
+  });
+
+  describe('when creating now artefacts using `playbook add`command', () => {
+    let playbookSubmitResult: RunCliResult;
+
+    beforeEach(async () => {
+      fs.mkdirSync(`${context.testDir}/.packmind/standards/`, {
+        recursive: true,
+      });
+      updateFile(
+        `.packmind/standards/my-standard.md`,
+        `# My new standard
+        
+With a description:
+
+* rule 1
+* rule 2
+`,
+        context.testDir,
+      );
+
+      await context.runCli(`playbook add .packmind/standards/my-standard.md`);
+      playbookSubmitResult = await context.runCli(
+        `playbook submit --no-review`,
+      );
+    });
+
+    it('succeeds', () => {
+      expect(playbookSubmitResult).toEqual({
+        returnCode: 0,
+        stderr: '',
+        stdout: expect.stringMatching('1 standard created'),
+      });
+    });
+
+    it('creates the new artefact', async () => {
+      const standards = await context.gateway.standards.list({
+        spaceId: context.space.id,
+      });
+
+      expect(standards).toEqual({
+        standards: [
+          expect.objectContaining({
+            name: 'My new standard',
+          }),
+        ],
+      });
+    });
+  });
+});
