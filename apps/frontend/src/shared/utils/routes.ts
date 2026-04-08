1e02ca261bb3603632e40bcef011259fece4eb22
Quentin Le Bourles <quentin.lebourles@promyze.com>
2026-04-08 13:27:07 +0200

    feat(frontend): add toJoinSpace route helper

--- apps/frontend/src/shared/utils/routes.ts
+++ apps/frontend/src/shared/utils/routes.ts
@@ -121,6 +121,8 @@ export const routes = {
       proposalId: string,
     ) =>
       `/org/${orgSlug}/space/${spaceSlug}/review-changes/${artefactType}/new/${proposalId}`,
+    toJoinSpace: (orgSlug: string, spaceId: string) =>
+      `/org/${orgSlug}/spaces/${spaceId}/join`,
   },
 
   /**
