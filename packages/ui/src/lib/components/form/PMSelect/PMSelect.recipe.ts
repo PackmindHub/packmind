947289ead04c89fe27439cba133e794e3e116437
Vincent Pretre <vincent.pretre@gmail.com>
2026-05-05 17:31:51 +0200

    Style PMSelect to match other inputs (combobox, input...)

--- packages/ui/src/lib/components/form/PMSelect/PMSelect.recipe.ts
+++ packages/ui/src/lib/components/form/PMSelect/PMSelect.recipe.ts
@@ -1,8 +1,17 @@
 import { defineSlotRecipe } from '@chakra-ui/react';
 
 export const pmSelectRecipe = defineSlotRecipe({
-  slots: [],
+  slots: ['trigger', 'content', 'item'],
   base: {
+    trigger: {
+      backgroundColor: '{colors.background.tertiary}',
+      borderColor: 'transparent',
+      _hover: { borderColor: 'transparent' },
+    },
+    content: {
+      borderColor: '{colors.border.tertiary}',
+      borderWidth: '1px',
+    },
     item: {
       _hover: {
         bg: '{colors.background.tertiary}',
