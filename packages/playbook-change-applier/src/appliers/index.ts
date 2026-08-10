6fccdf5e2e0a2abab2a40cc717d8f7bc2a6ca531
Vincent Pretre <vincent.pretre@gmail.com>
2026-08-10 10:18:16 +0200

    Drop unused files

--- packages/playbook-change-applier/src/appliers/index.ts
+++
@@ -1,4 +0,0 @@
-export type { IChangesProposalApplier } from './IChangesProposalApplier';
-export { StandardChangesApplier } from './StandardChangesApplier';
-export { CommandChangesApplier } from './CommandChangesApplier';
-export { SkillChangesApplier } from './SkillChangesApplier';
